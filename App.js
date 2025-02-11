4f77f371ce38544b1442d9e9a9047619c048f313
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-02-10 20:05:38 -0500

    ImageBackgound

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreens from './screens/StartGameScreens';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StartGameScreens />
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={['#ddb52f', '#4e0329']} style={styles.container}>
+      <ImageBackground
+        source={require('./assets/background.png')}
+        resizeMode="cover"
+        style={styles.container}
+        imageStyle={styles.backgroungImage}
+      >
+        <StartGameScreens />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    height: '100%',
+    flex: 1,
+  },
+  backgroungImage: {
+    opacity: 0.3,
   },
 });
